7121f7fdd9f3e2a8b8257acb6680601d2eb18e2b
verdandi <aimed.fire@gmail.com>
2015-09-03 14:16:23 +0300

    добавлен класс исключений. добравлена линковка библиотеки avutil

---
+++ sources/video_processing_error.h
@@ -0,0 +1,20 @@
+#ifndef VIDEO_PROCESSING_ERROR_H_
+#define VIDEO_PROCESSING_ERROR_H_
+
+#include <exception>
+#include <string>
+
+namespace VideoProcessing {
+
+class VideoProcessingError: public std::exception {
+public:
+    VideoProcessingError(const std::string& errorMessage): errorMessage_(errorMessage) {}
+    const char* what() const noexcept { return errorMessage_.c_str(); }
+private:
+    std::string errorMessage_;
+};
+
+}
+
+#endif /* end of include guard: VIDEO_PROCESSING_ERROR_H_ */
+
